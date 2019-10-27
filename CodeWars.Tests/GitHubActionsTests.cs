81d56ce2d1508cb1ec12d07b4c6d4ded895320a0
JiM <jmhumblet@gmail.com>
2019-10-27 10:44:10 +0100

    Add tests

---
+++ CodeWars.Tests/GitHubActionsTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars.Tests
+{
+    class GitHubActionsTests
+    {
+        [Test]
+        public void PassingTest()
+        {
+
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            Assert.Fail("FAIIIIIIL");
+        }
+    }
+}
